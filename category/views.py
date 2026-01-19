3b5f3c14f7480dca985b2fa9d28ac37ad8ad3208
Vivek Chandra Pandey (Vicky) <101510298+vickypandey14@users.noreply.github.com>
2026-01-19 23:10:01 +0530

    Refactor category view to improve readability

--- category/views.py
+++ category/views.py
@@ -1,15 +1,14 @@
 
 from django.shortcuts import render, redirect
 from . import forms
-# Create your views here.
 
 def category(request):
-    if request.method == 'POST': # user post request koreche
-        category_form = forms.CategoryForm(request.POST) # user er post request data ekhane capture korlam
-        if category_form.is_valid(): # post kora data gula amra valid kina check kortechi
-            category_form.save() # jodi data valid hoy taile database e save korbo
-            return redirect('category') # sob thik thakle take add author ei url e pathiye dibo
+    if request.method == 'POST':
+        category_form = forms.CategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return redirect('category')
     
-    else: # user normally website e gele blank form pabe
+    else:
         category_form = forms.CategoryForm()
     return render(request, 'category.html', {'form' : category_form})
